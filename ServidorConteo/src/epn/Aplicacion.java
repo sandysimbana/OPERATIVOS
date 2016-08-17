204adb196a5c20f7df5e80b199081c490d09f9e3
sandysimbana <sindy_5593@hotmail.com>
2016-08-16 20:11:31 -0500

    Update Aplicacion.java

--- ServidorConteo/src/epn/Aplicacion.java
+++ ServidorConteo/src/epn/Aplicacion.java
@@ -1,7 +1,34 @@
 
 package epn;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Aplicacion {
-    
+
+	public static void main(String[] args) {
+		
+		ServerSocket ss;
+        System.out.print("Inicializando servidor... ");
+        try {
+            ss = new ServerSocket(5555);
+            System.out.println("\t[OK]");
+            while (true) {
+                Socket socket;
+                socket = ss.accept();
+                System.out.println("Nueva conexión entrante: "+socket);
+                ((Servidor) new Servidor(socket)).start();
+              
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+		
+
+	}
+
 }
